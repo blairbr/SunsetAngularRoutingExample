c74dc927bd4dd0b57b8bc905069b1bec83ea0183
Blair Brown <blair.j.brown@gmail.com>
2021-01-20 20:12:53 -0500

    Sunset example from class

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+import { SunriseComponent } from './sunrise/sunrise.component';
+import { SunsetComponent } from './sunset/sunset.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // blank path is the default or home 
+  { path: '', component: HomeComponent }, 
+  { path: 'sunrise', component: SunriseComponent },
+  { path: 'sunrises', redirectTo: '/sunrise', pathMatch: 'full' },
+
+  { path: 'sunset', component: SunsetComponent },
+  //wildcard 
+  { path: '**', component: PagenotfoundComponent } //order matters
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/sunrise/sunrise.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sunrise',
+  templateUrl: './sunrise.component.html',
+  styleUrls: ['./sunrise.component.css']
+})
+export class SunriseComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
